8b9d9082809357b755473cfe2fb2bf1666731c25
salsabilasalimah <salsabila1852@gmail.com>
2025-09-09 01:19:33 +0700

    implementasi template dasar, implementasi model, migrasi model, mvt, routing url

--- main/models.py
+++ main/models.py
@@ -1,3 +1,28 @@
+# main/models.py
+
+import uuid
 from django.db import models
 
-# Create your models here.
+class Product(models.Model):
+
+    name = models.CharField(max_length=255)
+    price = models.IntegerField()  
+    description = models.TextField()
+    thumbnail = models.URLField(blank=True, null=True)
+    category = models.CharField(max_length=100)
+    is_featured = models.BooleanField()
+    stock = models.PositiveIntegerField(default=0)
+    colors = models.CharField(max_length=255, blank=True)
+
+    def __str__(self):
+        return self.name
+
+    def add_stock(self, quantity):
+        if quantity > 0:
+            self.stock += quantity
+            self.save()
+
+    def reduce_stock(self, quantity):
+        if 0 < quantity <= self.stock:
+            self.stock -= quantity
+            self.save()
--- main/views.py
+++ main/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+def show_main(request):
+    context = {
+        'npm' : '2406432734',
+        'name': 'Salsabila Salimah',
+        'class': 'PBP F'
+    }
+
+    return render(request, "main.html", context)
