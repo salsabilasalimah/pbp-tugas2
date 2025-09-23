5735b3a2c9c784b00dbb309c221d3898de9a056b
salsabilasalimah <salsabila1852@gmail.com>
2025-09-23 21:03:48 +0700

    revisi lagi

--- main/urls.py
+++ main/urls.py
@@ -17,7 +17,7 @@ urlpatterns = [
     path('', show_main, name='show_main'),
     path('create-product/', create_product, name='create_product'),
     path('product/<str:id>/', show_product, name='show_product'),
-    path("delete/<uuid:pk>/", views.delete_product, name="delete_product"),
+    path('delete/<uuid:pk>/', views.delete_product, name="delete_product"),
     path('login/', login_user, name='login'),
     path('logout/', logout_user, name='logout'),
     path('register/', register, name='register'),
@@ -27,6 +27,6 @@ urlpatterns = [
     path('json/<str:Product_id>/', show_json_by_id, name='show_json_by_id'),
 ]
 
-# Tambahkan ini untuk serve media files saat DEBUG = True
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# # Tambahkan ini untuk serve media files saat DEBUG = True
+# if settings.DEBUG:
+#     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
