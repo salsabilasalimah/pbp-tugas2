1524d6f97e8b0933d0e47e5d9d9b48a041e3f438
salsabilasalimah <salsabila1852@gmail.com>
2025-09-23 11:51:59 +0700

    Tugas Individu Selesai, namun sesi loginnya bermasalah

--- main/models.py
+++ main/models.py
@@ -1,9 +1,10 @@
 # main/models.py
 import uuid
 from django.db import models
+from django.contrib.auth.models import User
 
 class Product(models.Model):
-
+    
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = models.CharField(max_length=255)
     price = models.IntegerField()  
@@ -13,6 +14,7 @@ class Product(models.Model):
     is_featured = models.BooleanField(default=0)
     stock = models.PositiveIntegerField(default=0)
     views = models.PositiveIntegerField(default=0)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
 
     def __str__(self):
         return self.name
--- main/urls.py
+++ main/urls.py
@@ -1,7 +1,15 @@
-from django.urls import path
-from main.views import show_main, create_product, show_product, show_xml, show_json, show_xml_by_id, show_json_by_id
+from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from main.views import register
+from main.views import login_user
+from main.views import logout_user
+from . import views
+
+from main.views import (
+    delete_product, show_main, create_product, show_product,
+    show_xml, show_json, show_xml_by_id, show_json_by_id
+)
 
 app_name = 'main'
 
@@ -9,8 +17,16 @@ urlpatterns = [
     path('', show_main, name='show_main'),
     path('create-product/', create_product, name='create_product'),
     path('product/<str:id>/', show_product, name='show_product'),
+    path("delete/<uuid:pk>/", views.delete_product, name="delete_product"),
+    path('login/', login_user, name='login'),
+    path('logout/', logout_user, name='logout'),
+    path('register/', register, name='register'),
     path('xml/', show_xml, name='show_xml'),
     path('json/', show_json, name='show_json'),
-     path('xml/<str:product_id>/', show_xml_by_id, name='show_xml_by_id'),
-     path('json/<str:product_id>/', show_json_by_id, name='show_json_by_id'),
+    path('xml/<str:Product_id>/', show_xml_by_id, name='show_xml_by_id'),
+    path('json/<str:Product_id>/', show_json_by_id, name='show_json_by_id'),
 ]
+
+# Tambahkan ini untuk serve media files saat DEBUG = True
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
